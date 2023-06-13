9472be9ab753c49475adf93c7dbf65ddd004e02e
Ayush Mahatara <aym0168@my.londonmet.ac.uk>
2023-06-13 21:37:22 +0545

    creadte model a

--- .history/routes/api_20230613213448.php
+++ .history/routes/api_20230613213448.php
@@ -19,6 +19,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/products', 'ProductController');
-Route::group(['prefix' => 'products'], function () {
-    Route::apiResource('/{product/reviews', 'ReviewController');
-});
